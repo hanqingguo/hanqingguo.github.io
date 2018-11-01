f39da399734cf0494c4ea8642bde8c416fc95e40
hanqing <18523978636@163.com>
2018-11-01 08:42:39 -0400

    change

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   alert("hello world");
-if(window.location.href=="http://hanqingguo.github.io/")
+if(window.location.href=="http://hanqingguo.github.io")
 {
 $("#sidebar").css({width:'100%'});
 $("#btnblog").click(function(){
